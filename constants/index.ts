ac589190c610061f73bea97617b9cd7e478b7cd6
Himesh Parashar <himeshparashar424@gmail.com>
2024-04-28 12:53:50 +0530

    Update layout.tsx to add bg-dark-2 class Update package.json to add new dependencies Update globals.css to add dark mode styles

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sidebarLinks = [
+  {
+    label: "Home",
+    route: "/",
+    imgUrl: "/icons/something.svg",
+  },
+  {
+    label: "Upcomming",
+    route: "/upcomming",
+    imgUrl: "/icons/upcomming.svg",
+  },
+  {
+    label: "Previous",
+    route: "/previous",
+    imgUrl: "/icons/previous.svg",
+  },
+  {
+    label: "Recordings",
+    route: "/recordings",
+    imgUrl: "/icons/Video.svg",
+  },
+  {
+    label: "Personal Room",
+    route: "/personal-room",
+    imgUrl: "/icons/add-personal.svg",
+  },
+];
